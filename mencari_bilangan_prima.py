21ac609ac1be074c157513174e7882b8b0848430
Okek <87618530+skuyykek69@users.noreply.github.com>
2023-09-26 13:55:25 +0700

    Update and rename mencari_bilangan_prima to mencari_bilangan_prima.py
    tugas bocil

--- mencari_bilangan_prima.py
+++ mencari_bilangan_prima.py
@@ -1,7 +1,7 @@
 bilangan_awal=1
 bilangan_akhir=71
 
-for x in range(bilangan_awal, bilangan_akhir+1): #bisa juga langsung input (1, 71+1):
+for x in range(1, 71+1): #disini juga bisa langsung input bilangan nya misal| for x in range(1, 71+1):
     if x > 1:
         for y in range(2, x):
             if (x % y) == 0:
